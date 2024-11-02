e2b9678efd15566693c86856626076671d1ac857
Ольга Кузнецова <dljapythonplus@yandex.kz>
2024-11-02 15:36:21 +0100

    gfjrkdrf

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,20 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+class BirthdayAdmin(admin.ModelAdmin): 
+    list_display = ( 
+        'first_name', 
+        'last_name', 
+        'birthday',
+    ) 
+    list_editable = ( 
+        'birthday', 
+    ) 
+    search_fields = ('first_name',) 
+    list_filter = ('first_name',) 
+    list_display_links = ('first_name',) 
+    empty_value_display = 'Не задано'
+
+
+admin.site.register(Birthday, BirthdayAdmin)
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+# birthday/views.py 
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .forms import BirthdayForm
+# Импортируем из utils.py функцию для подсчёта дней.
+from .utils import calculate_birthday_countdown
+from .models import Birthday
 
 
-def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayMixin:
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
+
+
+# Добавляем миксин первым по списку родительских классов.
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    # Не нужно описывать атрибуты: все они унаследованы от BirthdayMixin.
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    # И здесь все атрибуты наследуются от BirthdayMixin.
+    pass  
+
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10 
